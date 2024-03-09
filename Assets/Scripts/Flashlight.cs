7a63972396944b49372b2809ee316742636f2135
Merve <meszaros@threaks.com>
2024-03-09 21:00:38 +0100

    Flashlight on/off and battery

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _flashlightText;
+    
+    public bool isFlashlightOn;
+
+    private float _flashLightIntensity;
+    private Light _flashLight;
+
+    private const float StartingIntensity = 5;
+    private const float IntensityDecayRate = 0.1f;
+
+    void Awake()
+    {
+        _flashLightIntensity = StartingIntensity;
+        _flashLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (_flashLightIntensity > 0f && isFlashlightOn)
+        {
+            _flashLightIntensity -= IntensityDecayRate * Time.deltaTime;
+            _flashLight.intensity = _flashLightIntensity;
+        }
+        else
+        {
+            _flashLight.intensity = 0f;
+        }
+        
+        if (isFlashlightOn)
+        {
+            var flashlightBattery = (int)(100 * _flashLightIntensity / StartingIntensity);
+            _flashlightText.text = $"Flashlight Battery: {flashlightBattery}%";
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isFlashlightOn = !isFlashlightOn;
+            
+            if(!isFlashlightOn)
+            {
+                _flashlightText.text = "Flashlight: OFF";
+            }
+
+            _flashLight.intensity = isFlashlightOn ? _flashLightIntensity : 0;
+        }
+    }
+}
